aee2c9a7d96d4525523f9452330ffb9b2779a97d
Pedro Henrique Mendes Pita <33984146+pedro-pita@users.noreply.github.com>
2017-11-29 16:58:59 +0000

    Incluir uma classe a parte na main

---
+++ 1ºModuloPHP/Classes/classeCarro.php
@@ -0,0 +1,45 @@
+<?php
+class Carro{
+    //aqui declaramos o atributo privado _marca
+    private $_marca;
+    //aqui declaramos o atributo privado _$_modelo
+    private $_modelo;
+    //aqui declaramos o atributo privado _cor
+    private $_cor;
+        
+    /*função publica de contrutor, é executada no momento de criação do objeto é executada no momento da criação do objeto */
+    public function __construct(){
+        echo 'Fui instanciado, logo que o objeto foi criado<br />';
+        /*O $this é usado para referenciar o proprio objeto, ou seja
+        * $this->marca por exemplo, estariamos a aceder ao atributo da classe carro */
+        $this->setAtributos(null,null,null);
+    }
+    public function __destruct(){
+        echo '<br />Objeto foi destruido.';
+    }
+    public function setAtributos($marca_p,$modelo_p,$cor_p){
+        //Atributo que 'seta' a marca com o parametro recebido $marca_p
+        $this->_marca = $marca_p;
+        //Atributo que 'seta' a cor com o parametro recebido $cor_p
+        $this->_cor=$cor_p;
+    }
+    public function getMarca(){
+        return $this->_marca;
+    }
+    public function setMarca($marca_p){
+        $this->_marca=$marca_p;
+    }
+    public function getModelo(){
+        return $this->_modelo;
+    }
+    public function setModelo($modelo_p){
+        $this->_modeloa=$modelo_p;
+    }
+    public function getCor(){
+        return $this->_cor;
+    }
+    public function setCor($cor_p){
+        $this->_cor=$cor_p;
+    }
+}
+?>
---
+++ 1ºModuloPHP/Classes/index.php
@@ -0,0 +1,43 @@
+<?php
+//incluir a classe Carro via ficheiros e na mesma raiz
+include 'classeCarro.php'; //importa a classe
+//criação de uma intancia da classe Carro
+$um_carro= new Carro();
+//criação de uma intancia da classe Carro
+$dois_carro= new Carro();
+//criação de uma intancia da classe Carro
+$tres_carro= new Carro();
+
+//chamar a função setAtributos, para atribuir ao objeto $um_carro
+$um_carro->setAtributos("Mercedes", "AMG", "azul");
+$dois_carro->setAtributos("Audi", "A6", "vermelho");
+$tres_carro->setAtributos("Peugeot", "308", "preto");
+
+//Exibir uma alinha de separação
+echo '<hr />'; //linha html de uma ponta a outra cinzenta com 1 px
+echo '<p>'; //paragrafo
+//Exibir a marca do carro, add quebra de linha <br />
+echo '<b>Carro: </b>'.$um_carro->getMarca().'<br />';
+echo '<b>Modelo: </b>'.$um_carro->getModelo().'<br />';
+echo '<b>Cor: </b>'.$um_carro->getCor().'<br />';
+echo '</p>';
+
+//Exibir uma alinha de separação
+echo '<hr />'; //linha html de uma ponta a outra cinzenta com 1 px
+echo '<p>'; //paragrafo
+//Exibir a marca do carro, add quebra de linha <br />
+echo '<b>Carro: </b>'.$dois_carro->getMarca().'<br />';
+echo '<b>Modelo: </b>'.$dois_carro->getModelo().'<br />';
+echo '<b>Cor: </b>'.$dois_carro->getCor().'<br />';
+echo '</p>';
+
+//Exibir uma alinha de separação
+echo '<hr />'; //linha html de uma ponta a outra cinzenta com 1 px
+echo '<p>'; //paragrafo
+//Exibir a marca do carro, add quebra de linha <br />
+echo '<b>Carro: </b>'.$tres_carro->getMarca().'<br />';
+echo '<b>Modelo: </b>'.$tres_carro->getModelo().'<br />';
+echo '<b>Cor: </b>'.$tres_carro->getCor().'<br />';
+echo '</p>';
+
+?>
